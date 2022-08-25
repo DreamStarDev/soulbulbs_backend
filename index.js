e600992aa55102ffc7ab985d77d6b3f733cfc1a0
yuri <yuri01051@outlook.com>
2022-08-25 21:55:02 +1000

    cors error fix

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ import router from './apis/apis.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    next();
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
