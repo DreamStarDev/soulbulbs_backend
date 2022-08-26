a0cfddf7120948f8c16e7e4bbf53c88da9ca6ec5
yuri <yuri01051@outlook.com>
2022-08-26 18:24:18 +1000

    fix addDna

--- apis/apis.js
+++ apis/apis.js
@@ -4,7 +4,7 @@ import DB from '../utils/DB.js';
 let router = express.Router();
 
 router.post('/checkDna', async function (req, res) {
-    let [data, fields] = await DB.DBQuery('SELECT * FROM dna_list WHERE dna=?', req.body.dna);
+    let [data, fields] = await DB.DBQuery('SELECT * FROM `dna_list` WHERE dna=?', [req.body.dna]);
     if (data.length == 0) {
         res.send({
             duplicated: false
@@ -17,18 +17,21 @@ router.post('/checkDna', async function (req, res) {
 });
 
 router.post('/addDna', async function (req, res) {
-    let [data, fields] = await DB.DBQuery('SELECT * FROM dna_list WHERE dna=?', req.body.dna);
+    let [data, fields] = await DB.DBQuery('SELECT * FROM `dna_list` WHERE dna=?', [req.body.dna]);
     if (data.length == 0) {
-        await DB.DBQuery('INSERT INTO `dna_list` (`dna`) VALUES(?)', req.body.dna);
-        res.send({
-            duplicated: false
-        });
-    } else {
-        res.send({
-            duplicated: true
-        });
+        await DB.DBQuery('INSERT INTO `dna_list` (`id`, `dna`) VALUES(?, ?)', [req.body.id, req.body.dna]);
     }
+    res.send({
+        success: true
+    });
 });
+
+router.get('/dnaList', async function (req, res) {
+    let [data, fields] = await DB.DBQuery('SELECT * FROM dna_list');
+    res.send({
+        dnaList: data
+    });
+})
 
 router.get('/likes', async function (req, res) {
     let [data, fields] = await DB.DBQuery('SELECT * FROM `like_list`');
