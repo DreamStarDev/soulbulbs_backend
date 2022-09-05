540f3cb01ad70be1cd341e72e7ef2435679ab340
SuperDev <yuri01051@outlook.com>
2022-09-05 17:49:18 +1000

    first commit

--- Config.js
+++ Config.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 const DevMode = process.env.DevMode;
 const DBHost = DevMode == "TRUE" ? "localhost" : "sql6.freemysqlhosting.net";
-const DBUser = DevMode == "TRUE" ? "root" : "sql6514933";
-const DBPassword = DevMode == "TRUE" ? "" : "dTWFFg9nlD";
-const DBName = DevMode == "TRUE" ? "soulbulbs" : "sql6514933";
+const DBUser = DevMode == "TRUE" ? "root" : "sql6517316";
+const DBPassword = DevMode == "TRUE" ? "" : "sxC4AM4ZC1";
+const DBName = DevMode == "TRUE" ? "soulbulbs" : "sql6517316";
 
 export { DevMode, DBHost, DBUser, DBPassword, DBName };
